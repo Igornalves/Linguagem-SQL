8969e4f573e5d40e92ed65dfb657c621e94cdf72
igor_alves <igornalves08@gmail.com>
2024-01-15 16:48:17 -0300

    iniciando reepositorio de aprendizado da linguagem SQL no Postgres

---
+++ SQL/INSERT INTO (aprendendo_sql).sql
@@ -0,0 +1,7 @@
+INSERT INTO testedetabela (id, primeironome, segundonome, idade)
+VALUES (
+        id :integer,
+        'primeironome:character varying',
+        'segundonome:character varying',
+        idade :integer
+    );
